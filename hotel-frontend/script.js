e6e624e22225d0cc0b3678e491403e9aad3576f9
Fredrik Welander <welandfr@arcada.fi>
2026-04-01 09:43:19 +0300

    add hotel frontend

---
+++ hotel-frontend/script.js
@@ -0,0 +1,17 @@
+//const apiUrl = "https://wdb26-exempel-deployment-testing.2.rahtiapp.fi/api/ip";
+const apiUrl = "http://127.0.0.1:8080";
+
+async function getRooms() {
+    const res = await fetch(`${apiUrl}/rooms`);
+    const rooms = await res.json();
+
+    console.log(rooms)
+
+    for (room of rooms) {
+        document.getElementById("room-list").innerHTML += `
+            <li>${room.room_number} - ${room.room_type} - ${room.price} €</li>
+        `;
+    }
+    
+}
+getRooms();
